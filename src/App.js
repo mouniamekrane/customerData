2e73359ccaed96d9e1f172ab436fa3f89acf33fa
mouniamekrane <mouniamekrane@gmail.com>
2022-01-13 01:32:51 +0100

    The active Customer

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import './App.css';
 import { TableBodyClients } from "./Table/Table.js"
-import { TableFooter } from "./Footer/footer.js"
 function App() {
   return (
     <div className="App" >
       <TableBodyClients />
-      <TableFooter />
     </div>
   );
 }
--- src/Footer/ActiveClients/activeClients.js
+++ src/Footer/ActiveClients/activeClients.js
@@ -1,8 +1,15 @@
-
 import './activeClients.css'
-export const CountActive = ({ clientsToCount }) => {
-    return clientsToCount.filter((client) => {
-        return client.status === "active";
-    }).length;
-    ;
+export const CountActive = ({ ClientsToCount }) => {
+    const ActiveArray = []
+    ClientsToCount.filter((client) => {
+        if (client.status === 'active') {
+            ActiveArray.push(client)
+            return ActiveArray.length
+        }
+    })
+    return (
+        <div className="clients" id="actived-clients">
+            ACTIVE CUSTOMERS :<span className='ActiveArray'>{ActiveArray.length}</span> / <span>{ClientsToCount.length}</span>
+        </div>
+    )
 }
